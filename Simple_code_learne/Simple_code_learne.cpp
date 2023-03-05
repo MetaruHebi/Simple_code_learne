de861e2c448b181e4e8eefeffefb1b7442b03e99
Morpheas <Morpheas@DESKTOP-KE7BTRP>
2023-03-05 11:12:46 +0300

    Добавьте файлы проекта.

---
+++ Simple_code_learne/Simple_code_learne.cpp
@@ -0,0 +1,26 @@
+﻿// Simple_code_learne.cpp: определяет точку входа для приложения.
+//
+
+#include "Simple_code_learne.h"
+#include <iostream>
+using namespace std;
+
+void FillArray(int* const arr, const int size) {
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10;
+    }
+}
+
+void ShowArray(int* const arr, const int size) {
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << "\t";
+    }
+}
+
+void main() {
+
+    FillArray();
+
+}
