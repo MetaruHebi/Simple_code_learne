bbcb63cd6a1bb605cfdf80829d52f98af60c67ae
Morpheas <Morpheas@DESKTOP-KE7BTRP>
2023-03-06 12:35:01 +0300

    Output of dynamic arrays

--- Simple_code_learne/Simple_code_learne.cpp
+++ Simple_code_learne/Simple_code_learne.cpp
@@ -17,10 +17,24 @@ void ShowArray(int* const arr, const int size) {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << "\t";
     }
+    cout << endl;
 }
 
 void main() {
 
-    FillArray();
+    int size = 10;
+    int* firstArray = new int[size];
+    int* secondArray = new int[size];
 
+    FillArray(firstArray, size);
+    FillArray(secondArray, size);
+
+    cout << "First Array = ";
+    ShowArray(firstArray, size);
+    cout << "Second Array = ";
+    ShowArray(secondArray, size);
+
+    delete[] secondArray;
+    delete[] firstArray;
 }
+
